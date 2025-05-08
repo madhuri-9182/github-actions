c7235381b98ccd3246493b1a224c050fa30f2fd4
madhuri-9182 <madhurisomannagari@gmail.com>
2025-05-08 19:32:05 +0530

    changes

--- src/addition.py
+++ src/addition.py
@@ -2,7 +2,10 @@
 # This is a test commit
 def add(a, b):
     return a + b
+    print(f"{a} + {b} = {result}")
+    return result
 
 def test_add():
     assert add(1, 2) == 3
     assert add(1, -1) == 0
+    
